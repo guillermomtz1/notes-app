29694f6dbfeaeb8e9f25e62570798d81deb87bfa
guillermo <jguillermo.martinez1@gmail.com>
2025-09-09 20:11:54 -0500

    Add debug panel back to fix deployed subscription status
    - Adds SubscriptionDebug component to both premium and free user views
    - Allows manual subscription update via debug panel in deployed app
    - Will be removed after subscription status is properly fixed

--- frontend/notes-app/src/pages/Subscription/Subscription.jsx
+++ frontend/notes-app/src/pages/Subscription/Subscription.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { FaCrown, FaCheck, FaArrowLeft } from "react-icons/fa";
 import SubscriptionUpgrade from "../../components/Subscription/SubscriptionUpgrade";
 import SubscriptionCancel from "../../components/Subscription/SubscriptionCancel";
+import SubscriptionDebug from "../../components/Admin/SubscriptionDebug";
 import { API_ENDPOINTS, apiRequest } from "../../utils/api";
 
 const Subscription = () => {
@@ -167,6 +168,11 @@ const Subscription = () => {
             <div className="card">
               <SubscriptionCancel />
             </div>
+
+            {/* Debug Panel - Use to fix subscription status */}
+            <div className="card">
+              <SubscriptionDebug />
+            </div>
           </div>
         ) : (
           /* Free User View */
@@ -258,6 +264,11 @@ const Subscription = () => {
                   ></div>
                 </div>
               </div>
+            </div>
+
+            {/* Debug Panel - Use to fix subscription status */}
+            <div className="card">
+              <SubscriptionDebug />
             </div>
           </div>
         )}
