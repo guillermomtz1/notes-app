ac321b25360d575a6a7f2b86b600fcf405641535
guillermo <jguillermo.martinez1@gmail.com>
2025-09-09 20:07:28 -0500

    Remove SubscriptionDebug panel from deployed app
    - Removes the debug panel that was showing in production
    - Removes unused import of SubscriptionDebug component
    - Keeps the component file for future development use

--- frontend/notes-app/src/pages/Subscription/Subscription.jsx
+++ frontend/notes-app/src/pages/Subscription/Subscription.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { FaCrown, FaCheck, FaArrowLeft } from "react-icons/fa";
 import SubscriptionUpgrade from "../../components/Subscription/SubscriptionUpgrade";
 import SubscriptionCancel from "../../components/Subscription/SubscriptionCancel";
-import SubscriptionDebug from "../../components/Admin/SubscriptionDebug";
 import { API_ENDPOINTS, apiRequest } from "../../utils/api";
 
 const Subscription = () => {
@@ -259,11 +258,6 @@ const Subscription = () => {
                   ></div>
                 </div>
               </div>
-            </div>
-
-            {/* Debug Panel - Remove this after testing */}
-            <div className="card">
-              <SubscriptionDebug />
             </div>
           </div>
         )}
